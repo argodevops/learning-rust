914541e556bcf0b1178a7dea42328599389e206c
Chris F <58433277+chris-argo@users.noreply.github.com>
2024-02-01 08:56:47 +0000

    Update main.rs
    Signed-off-by: Chris F <58433277+chris-argo@users.noreply.github.com>

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}, {}!", "Hello", "cargo"); // Hello, cargo!
+    println!("{0}, {1}!", "Hello", "cargo"); // Hello, cargo!
+    println!("{greeting}, {name}!", greeting = "Hello", name = "cargo"); // Hello, cargo!
+
+    let (greeting, name) = ("Hello", "cargo"); // 💡 Two Variable bindings declare & initialize in one line.
+    println!("{greeting}, {name}!"); // Hello, cargo!
+
+    println!("{:?}", [1, 2, 3]); // [1, 2, 3]
+    println!("{:#?}", [1, 2, 3]);
+    /*
+        [
+            1,
+            2,
+            3
+        ]
+    */
+
+    // 🔎 The format! macro is used to store the formatted string.
+    let x = format!("{}, {}!", "Hello", "cargo");
+    println!("{}", x); // Hello, cargo!
+
+    // 💡 Rust has a print!() macro as well
+    print!("Hello, cargo!"); // Without new line
+    println!(); // A new line
+
+    print!("Hello, cargo!\n"); // With new line
 }
